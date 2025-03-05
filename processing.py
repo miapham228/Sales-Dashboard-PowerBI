52cba3e2197a18e17760ffd509281499733eed5c
Mia Pham <115346112+miapham228@users.noreply.github.com>
2025-03-05 14:36:31 +1300

    Add files via upload

--- processing.py
+++ processing.py
@@ -6,9 +6,15 @@ df = pd.read_excel("C:/Users/Tuyet.Pham/Downloads/SALE REPORT.xlsx")
 
 # remove columns
 rev_cols = [col for col in df.columns if col.startswith("Rev ")]
+
+# If want to have revenue of last month
+# latest_revs = sorted(rev_cols, reverse=True)[1:2]
+# for revenue of current month
 latest_revs = sorted(rev_cols, reverse=True)[:1]
+
 other_cols = [col for col in df.columns if not col.startswith(("Rev ", "Items ", "Total "))]
 df_filtered = df[other_cols + latest_revs]
+
 
 # Pivot the table (convert columns to rows)
 df_melted = df_filtered.melt(id_vars=other_cols, 
@@ -16,10 +22,12 @@ df_melted = df_filtered.melt(id_vars=other_cols,
                              var_name="Category", 
                              value_name="Value")
 
-# Save or display the result
-# df_melted.to_excel("pivoted_file.xlsx", index=False)
 
-#print(df_melted.head())  # Display first few rows
+# Save or display the result
+
 df_melted["Value"] = pd.to_numeric(df_melted["Value"], errors="coerce")
-print(df_melted["Value"].sum())  # Sum of revenue values
-df_melted.to_excel("C:/Users/Tuyet.Pham/Downloads/revenue.xlsx", index=False)
+df_melted.dropna(subset = ['Value'], inplace = True) # remove rows with NA values in Value column
+
+print(df_melted["Value"].sum())  # Sum of revenue values for cross check with Excel file
+
+df_melted.to_excel("C:/Users/Tuyet.Pham/OneDrive - FEL/Desktop/PROJECTS/Sales Dashboard/pythonwork/revenue_Mar25.xlsx", index=False)
