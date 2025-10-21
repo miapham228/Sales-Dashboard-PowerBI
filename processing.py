29de63359230b3b3b4c50381c6e9a7055bd91cf5
Mia Pham <115346112+miapham228@users.noreply.github.com>
2025-10-22 11:32:15 +1300

    Add files via upload

--- processing.py
+++ processing.py
@@ -1,18 +1,14 @@
 import pandas as pd
 
-# HAVE TO COMMENT on 1 of lates_revs
-
 # loading data
 df = pd.read_excel("C:/Users/Tuyet.Pham/Downloads/SALE_REPORT.xlsx")
-# df1 = pd.read_excel("C:/Users/Tuyet.Pham/OneDrive - FEL/Desktop/PROJECTS/Sales Dashboard/pythonwork/Sale Transaction Historical Data.xlsx")
+df1 = pd.read_excel("C:/Users/Tuyet.Pham/OneDrive - FEL/Desktop/PROJECTS/Sales Dashboard/pythonwork/Sale Transaction Historical Data.xlsx", dtype =str )
 
 # list of all Rev columns from sale report
-# rev_cols = [col for col in df.columns if col.startswith("Rev ")]
-# print(rev_cols)
-# revenue for HISTORICAL DATA (everytime when we in new month)
-# latest_revs = sorted(rev_cols, reverse=True)[1]
+rev_cols = [col for col in df.columns if col.startswith("Rev ")]
 
-latest_revs = "Rev 202508"
+# print(rev_cols)
+latest_revs = "Rev 202509"
 other_cols = [col for col in df.columns if not col.startswith(("Rev ", "Items ", "Total "))]
 df_filtered = df[other_cols + [latest_revs]]
 
@@ -32,11 +28,32 @@ df_melted.dropna(subset = ['Value'], inplace = True) # remove rows with NA value
 print(df_melted["Value"].sum())  # Sum of revenue values for cross check with Excel file
 
 # create current sale in python folder
-df_melted.to_excel("C:/Users/Tuyet.Pham/OneDrive - FEL/Desktop/PROJECTS/Sales Dashboard/pythonwork/Sale Transaction Current Month Data.xlsx", index=False)
-df_melted.to_excel("C:/Users/Tuyet.Pham/OneDrive - FEL/Desktop/PROJECTS/Sales Dashboard/pythonwork/revenue_202508.xlsx", index=False)
+# df_melted.to_excel("C:/Users/Tuyet.Pham/OneDrive - FEL/Desktop/PROJECTS/Sales Dashboard/pythonwork/Sale Transaction Current Month Data.xlsx", index=False)
+# df_melted.to_excel("C:/Users/Tuyet.Pham/OneDrive - FEL/Desktop/PROJECTS/Sales Dashboard/pythonwork/revenue_202508.xlsx", index=False)
 
 
-# append to historical data
+# only run this when new month comes, remember to choose correct monthname in line 11 (latest_revs)
 # df1 = pd.concat([df1, df_melted], ignore_index=True)
 # df1.to_excel("C:/Users/Tuyet.Pham/OneDrive - FEL/Desktop/PROJECTS/Sales Dashboard/pythonwork/Sale Transaction Historical Data.xlsx", index=False)
+
+
+# checking duplicate records in Customer No and Customer Name
+
+# df1 = df1[['Customer No', 'Customer Name']]
+# # Step 2: Group by customer number and count unique names
+# name_counts = df1.groupby('Customer No')['Customer Name'].nunique()
+
+# # Filter for customer numbers with more than one name
+# filtered_counts = name_counts[name_counts > 1]
+
+# # Convert to DataFrame and reset index
+# result_df = filtered_counts.reset_index()
+# result_df.columns = ['customer_number', 'name_count']
+
+# # Step 5: Export to Excel
+# result_df.to_excel('customers_with_multiple_names.xlsx', index=False)
+
+
+
+
 
